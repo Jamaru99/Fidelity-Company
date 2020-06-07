b124bfb99bcc01b6122f9e39f9f8108afe84cbf0
iDownward <matheus-1199@hotmail.com>
2020-06-07 15:39:00 -0300

    initial setup

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,179 +1,87 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { TextField } from 'react-native-material-textfield';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions
+} from 'react-native';
+// import { authenticate as authenticateCustomer } from '@state/action'
+//import { authenticate as authenticateCompany } from '@state/company/company.action'
+// import { window } from '@constants';
+import texts from '../utils/text';
 
-import { MonoText } from '../components/StyledText';
+const window = Dimensions.get('window')
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+export default function LoginScreen({navigation, authenticateCustomer, data}) {
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
+    const onLoginClick = () => {
+        // authenticateCustomer({username, password, navigation});
+        //authenticateCompany({username, password, navigation });
+    }
 
     return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
+        <View style={styles.container}>
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
+            <TextField
+                label={texts.login["placeholder:username"]}
+                onChangeText={(text) => setUsername(text)}
+            />
+            <TextField
+                label={texts.login["placeholder:password"]}
+                secureTextEntry={true}
+                onChangeText={(text) => setPassword(text)}
+            />
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
+            <Button 
+                title={texts.login["button:login"]} 
+                onPress={onLoginClick} 
+                disabled={!username || !password}
+            />
+
+            {/* <Text>{data.error}</Text> */}
+
+            <View style={styles.registerContainer}>
+                <Text>{texts.login["new_user"]} </Text>
+                <TouchableOpacity onPress={_ => navigation.navigate("Register")}>
+                  <Text style={styles.registerText}>{texts.login["link:sign_up"]}</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: window.height,
+    width: window.width/1.2,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    paddingBottom: 80
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  registerContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center'
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  registerText: {
+    color: 'blue',
+    textDecorationLine: 'underline'
+  }
 });
+
+// const mapStateToProps = ({ reducer }) => ({
+//     data: reducer.data,
+// });
+  
+// const mapDispatchToProps = dispatch =>
+//   bindActionCreators({ authenticateCustomer }, dispatch);
+  
+// export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
